cde6c0f01083c3396f67b5e2d228c3093c357ecd
Marcus Stong <stongo@gmail.com>
2015-08-26 22:10:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var Model = require('wadofgum');
+var messageSchema = require('../schemas/ci-message');
+var Joi = require('joi');
+var uuid = require('uuid');
+
+var messageSchema = {
+    id: Joi.string().default(uuid.v1, 'unique id'),
+    source: Joi.string().required(),
+    timestamp: Joi.date().format('x').default(Date.now, 'timestamp'),
+    reference: Joi.string().default(''),
+    payload: Joi.any().optional()
+};
+
+var Message = new Factory({
+    type: 'event',
+    schema: messageSchema
+});
+
+module.exports = Message;
